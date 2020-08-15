02a936fd3f13c773c554cc32d70b28a3442e7d9f
cAttte <26514199+cAttte@users.noreply.github.com>
2020-08-15 20:26:47 -0300

    build(scripts/build): add style metadata to result css

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,7 @@
 const fs = require("fs")
 const chalk = require("chalk")
 const sass = require("node-sass")
+const userCSS = require("usercss-meta")
 
 const STYLE = process.argv[2]
 if (!fs.existsSync(`./src/${STYLE}/style.scss`))
@@ -11,6 +12,12 @@ sass.render({ file: `./src/${STYLE}/style.scss`, outputStyle: "compressed" }, (e
         process.exit(console.log(chalk.red(err.formatted)))
     if (!fs.existsSync("./dist"))
         fs.mkdirSync("./dist")
-    fs.writeFileSync(`./dist/${STYLE}.min.css`, result.css)
+
+    const defaultData = JSON.parse(fs.readFileSync("./src/defaults.json"))
+    const styleData = JSON.parse(fs.readFileSync(`./src/${STYLE}/style.json`))
+    const data = { ...defaultData, ...styleData }
+    const meta = userCSS.stringify(data, { alignKeys: true })
+    fs.writeFileSync(`./dist/${STYLE}.min.css`, `${meta}\n\n${result.css}`)
+
     console.log(chalk.green(`Compiled ${STYLE}.`))
 })
