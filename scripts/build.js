bd4d277e17b0163192e4ab80416c20e7e6c66b76
cAttte <26514199+cAttte@users.noreply.github.com>
2020-08-15 20:33:56 -0300

    build(scripts/build): make build destination dir <style>/dist/

--- scripts/build.js
+++ scripts/build.js
@@ -10,14 +10,14 @@ if (!fs.existsSync(`./styles/${STYLE}/style.scss`))
 sass.render({ file: `./styles/${STYLE}/style.scss`, outputStyle: "compressed" }, (err, result) => {
     if (err)
         process.exit(console.log(chalk.red(err.formatted)))
-    if (!fs.existsSync("./dist"))
-        fs.mkdirSync("./dist")
+    if (!fs.existsSync(`./styles/${STYLE}/dist/`))
+        fs.mkdirSync(`./styles/${STYLE}/dist/`)
 
     const defaultData = JSON.parse(fs.readFileSync("./styles/defaults.json"))
     const styleData = JSON.parse(fs.readFileSync(`./styles/${STYLE}/style.json`))
     const data = { ...defaultData, ...styleData }
     const meta = userCSS.stringify(data, { alignKeys: true })
-    fs.writeFileSync(`./dist/${STYLE}.min.css`, `${meta}\n\n${result.css}`)
+    fs.writeFileSync(`./styles/${STYLE}/dist/${STYLE}.min.css`, `${meta}\n\n${result.css}`)
 
     console.log(chalk.green(`Compiled ${STYLE}.`))
 })
