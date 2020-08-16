2ccfa4f107196fdb2868855dc22d341097fa538a
cAttte <26514199+cAttte@users.noreply.github.com>
2020-08-15 23:37:10 -0300

    build(scripts/watch): import build function instead of spawning process

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,21 +1,17 @@
 const chalk = require("chalk")
-const { exec } = require("child_process")
 const fs = require("fs")
 const chokidar = require("chokidar")
+const build = require("./build")
 
-const STYLE = process.argv[2]
-const path = `./styles/${STYLE}`
+const style = process.argv[2]
+const path = `./styles/${style}`
 if (!fs.existsSync(path))
     process.exit(chalk.red(`${path} does not exist.`))
 
 const start = new Date()
-console.log(chalk.green(`Watching for changes in ${STYLE}.`))
-chokidar.watch(path, { ignored: /dist/ })
-    .on("all", (event, path) => {
-        if (new Date() - start < 100) return
-
-        console.log(`${chalk.yellow(event)}: ${path}`)
-        exec(`npm run build -- ${STYLE}`).on("close", () =>
-            console.log(chalk.green(`Compiled ${STYLE}.`))
-        )
-    })
+console.log(chalk.green(`Watching for changes in ${style}.`))
+chokidar.watch(path, { ignored: /dist/ }).on("all", (event, path) => {
+    if (new Date() - start < 100) return
+    console.log(`${chalk.yellow(event)}: ${path}`)
+    setTimeout(build, 100, style)
+})
