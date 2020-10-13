8ff1a59433a936ad9b67273b6aec751b6ee70de2
cAttte <26514199+cAttte@users.noreply.github.com>
2020-10-13 16:57:00 -0300

    refactor/style(scripts/build): use async methods, change some stuff

--- scripts/build.js
+++ scripts/build.js
@@ -1,30 +1,37 @@
-const fs = require("fs")
+const fs = require("fs/promises")
 const chalk = require("chalk")
 const sass = require("node-sass")
 const render = require("util").promisify(sass.render)
 const userCSS = require("usercss-meta")
 
 async function build(style) {
-    if (!fs.existsSync(`./styles/${style}/style.scss`))
-        console.log(chalk.red(`Error: "styles/${style}/style.scss" does not exist.`))
+    await fs.access(`./styles/${style}/style.scss`).catch(() => {
+        console.log(
+            chalk.red(`Error: style ${chalk.redBright(`"${style}"`)} does not exist.`)
+        )
+        process.exit()
+    })
 
     let error = null
     const result = await render({
         file: `./styles/${style}/style.scss`,
         outputStyle: "compressed"
-    }).catch(err => error = err)
+    }).catch(err => (error = err))
     if (error) return console.log(chalk.red(error.formatted))
 
-    if (!fs.existsSync(`./styles/${style}/dist/`))
-        fs.mkdirSync(`./styles/${style}/dist/`)
+    await fs
+        .access(`./styles/${style}/dist/`)
+        .catch(() => fs.mkdir(`./styles/${style}/dist/`))
 
-    const data = userCSS.stringify({
-        ...JSON.parse(fs.readFileSync("./styles/defaults.json")),
-        ...JSON.parse(fs.readFileSync(`./styles/${style}/style.json`))
-    }, { alignKeys: true })
-    fs.writeFileSync(`./styles/${style}/dist/${style}.min.css`, `${data}\n\n${result.css}`)
+    const defaults = JSON.parse(await fs.readFile("./styles/defaults.json"))
+    const data = JSON.parse(await fs.readFile(`./styles/${style}/style.json`))
+    const meta = userCSS.stringify({ ...defaults, ...data }, { alignKeys: true })
+    await fs.writeFile(
+        `./styles/${style}/dist/${style}.min.css`,
+        `${meta}\n\n${result.css}`
+    )
 
-    console.log(chalk.green(`Compiled ${style}.`))
+    console.log(chalk.green(`Compiled ${chalk.greenBright(style)}.`))
 }
 
 build(process.argv[2])
