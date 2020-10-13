e9044924ca577893c323a3c643d97458d08f5af7
cAttte <26514199+cAttte@users.noreply.github.com>
2020-10-13 17:07:40 -0300

    refactor/style(scripts/watch): use async method, change some stuff

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,17 +1,23 @@
 const chalk = require("chalk")
-const fs = require("fs")
+const fs = require("fs/promises")
 const chokidar = require("chokidar")
 const build = require("./build")
 
-const style = process.argv[2]
-const path = `./styles/${style}`
-if (!fs.existsSync(path))
-    process.exit(chalk.red(`${path} does not exist.`))
+async function watch(style) {
+    const path = `./styles/${style}`
+    await fs.access(path).catch(() => {
+        chalk.red(`${path} does not exist.`)
+        process.exit()
+    })
 
-const start = new Date()
-console.log(chalk.green(`Watching for changes in ${style}.`))
-chokidar.watch(path, { ignored: /dist/ }).on("all", (event, path) => {
-    if (new Date() - start < 100) return
-    console.log(`${chalk.yellow(event)}: ${path}`)
-    setTimeout(build, 100, style)
-})
+    const start = new Date()
+    console.log(chalk.yellow(`Watching for changes in ${chalk.yellowBright(style)}.`))
+    chokidar.watch(path, { ignored: /dist/ }).on("all", (event, path) => {
+        if (new Date() - start < 100) return
+        console.log(`${chalk.yellow(event) + ":"} ${chalk.yellowBright(path)}`)
+        setTimeout(build, 100, style)
+    })
+}
+
+watch(process.argv[2])
+module.exports = watch
